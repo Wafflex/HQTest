c135c20d6c0b60e2b3c2abb716c4e8a51d04e628
Jhonny Piñango <jhonnyleandro1@gmail.com>
2019-02-20 18:20:43 -0400

    Cron every hour

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('cookies:spam')
-                   ->daily();
+                   ->hourly();
     }
 
     /**
